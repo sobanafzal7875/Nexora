260b3caac004f2a22af6a7f308d1a20344073c75
Malik Awais <soban.afzal2004@gmail.com>
2026-05-27 23:51:05 +0500

    Deployment fix

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const { loadEnvConfig } = nextEnv;
 loadEnvConfig(process.cwd());
 
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = 'localhost';
-const port = 3000;
+const hostname = process.env.HOSTNAME || '0.0.0.0';
+const port = process.env.PORT || 3000;
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
